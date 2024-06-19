45949feb7d70136744ee97572fed49bb712e1897
avelasquez-pl <avelasquez-pl>
2024-06-19 22:23:05 +0000

    create bronze notebooks
    Create notebooks to pull data from primary database and push it into the bronze schema in Delta Lake

---
+++ medallion/bronze/primarydb.app.job.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC %run "/Workspace/Users/andres.velasquez@bosscathome.com/utils/utils"
+
+# COMMAND ----------
+
+connection_config = {
+    "host": dbutils.secrets.get(scope="devprimarydb", key="host"),
+    "port": dbutils.secrets.get(scope="devprimarydb", key="port"),
+    "database": dbutils.secrets.get(scope="devprimarydb", key="database"),
+    "username": dbutils.secrets.get(scope="devprimarydb", key="username"),
+    "password": dbutils.secrets.get(scope="devprimarydb", key="password"),
+}
+
+# COMMAND ----------
+
+table_name = "app.job"
+
+# COMMAND ----------
+
+remote_table = load_table_from_database(connection_config, table_name)
+
+# COMMAND ----------
+
+write_table_to_bronze(remote_table, table_name)
